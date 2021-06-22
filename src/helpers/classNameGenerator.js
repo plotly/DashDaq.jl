717dd50110a58e5ce3f6c240e5db1054a10ddc72
Joseph Damiba <joseph.damiba@plot.ly>
2021-06-22 14:51:21 -0400

    remove non julia files

--- src/helpers/classNameGenerator.js
+++
@@ -1,7 +0,0 @@
-import { omit } from 'ramda';
-
-export const getClassName = (componentName, theme) => {
-  return 'daq-' + componentName + (theme && theme.dark ? '--dark' : '--light');
-};
-
-export const getFilteredProps = props => omit(['className', 'id', 'setProps'], props);
--- src/styled/shared/Label.styled.js
+++
@@ -1,8 +0,0 @@
-import styled, { css } from 'styled-components';
-
-export default styled.label`
-  display: block;
-  font-size: 14px;
-  ${ ({position}) => position === 'top' ? css`margin-bottom: 8px;` : css`margin-top: 8px;` }
-  ${props => css`${props.css}`}
-`;
